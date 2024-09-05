560b7fd3b4e789d33ea68b67d1ba5f4fcfb847d3
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-09-05 20:58:46 +0700

    Variables, Strings and Integers

--- main.go
+++ main.go
@@ -3,5 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello! This is my first time in Golang")
+	// Strings
+	var nameOne string = "Nyan"
+	var nameTwo = "Lin"
+	nameThree := "Htoo" //shorthand
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	// Integer
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40 // shorthand
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits and momery
+	var numOne int8 = 25
+	var numTwo int8 = -128
+	var numThree uint16 = 21546 //unsigned Integer (contain zero or positive integer)
+	fmt.Println(numOne, numTwo, numThree)
+
+	// Float
+	var scoreOne float32 = 25.9
+	scoreTwo := 685865.3325 // shorthand (default float64)
+	fmt.Println(scoreOne, scoreTwo)
+
 }
