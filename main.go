72cd30ad9f2a0f0887a7e06e57daa55c0c17f816
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-09-12 16:30:35 +0700

    Learned Loops

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	// variables()
 	// printFormatString()
 	// arraysAndSlices()
-	standardLibrary()
+	// standardLibrary()
+	loops()
 }
 
 func variables() {
@@ -115,4 +116,31 @@ func standardLibrary() {
 	fmt.Println(names)
 
 	fmt.Println(sort.SearchStrings(names, "nyan"))
+}
+
+func loops() {
+	x := 0
+	for x < 5 {
+		fmt.Println("the value of x : ", x)
+		x++
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("the value of i : ", i)
+	}
+
+	names := []string{"nyan", "lin", "htoo", "furtive"}
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
+
+	for index, value := range names {
+		fmt.Printf("The value at index is %v and %v position \n", index, value)
+	}
+
+	// if we don't need index, use "underscore"
+	for _, value := range names {
+		fmt.Printf("The value is %v \n", value)
+		value := "new String" // nothing happened because "value" is local copy of variable
+	}
 }
